84c72b2a74565bd26549235b21c39d77ad5460b9
huangwt <huangwt@liandisys.com.cn>
2018-02-24 16:55:35 +0800

    update index.js

--- citymanagement/src/Index.js
+++ citymanagement/src/Index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Login from './Login';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
